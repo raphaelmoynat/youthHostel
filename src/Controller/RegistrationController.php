c2a19c91712397209e454834255a2217f62272d2
raphael <raphael.moynat@gmail.com>
2024-10-03 15:12:47 +0200

    initial commit

---
+++ src/Controller/RegistrationController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\RegistrationFormType;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\SerializerInterface;
+
+class RegistrationController extends AbstractController
+{
+    #[Route('/register', name: 'app_register', methods: ['POST'])]
+    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager, SerializerInterface $serializer, UserRepository $userRepository): Response
+    {
+        $user = $serializer->deserialize($request->getContent(), User::class, 'json');
+
+        $userExists = $userRepository->findOneBy(["username"=>$user->getUsername()]);
+
+        if ($userExists) {
+            return $this->json("username already exists", 300);
+        }
+
+
+        /** @var string $plainPassword */
+        $plainPassword = $user->getPassword();
+
+        // encode the plain password
+        $user->setPassword($userPasswordHasher->hashPassword($user, $plainPassword));
+
+        $entityManager->persist($user);
+        $entityManager->flush();
+
+        // do anything else you need here, like send an email
+
+        return $this->json($user, Response::HTTP_CREATED, [], ['groups' => ['userjson']]);
+
+
+
+    }
+}
